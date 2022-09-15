d0b6fae8c50d91c7262fa54ea5bfdd513cb6b683
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-15 15:39:44 -0300

    Delete bundle.js

--- bundle.js
+++
@@ -1,9 +0,0 @@
-//npm install -D typescript, tem q instalar no node, o -D só para desenvolvimento
-var idade = 27;
-var nome = 'Diogo';
-var maiorDeIdade = true;
-function soma(num1, num2) {
-    return num1 + num2;
-}
-document.write("A soma de 7 + 5 \u00E9 ".concat(soma(7, 5)));
-//comando para transpilar o typeScript tsc
