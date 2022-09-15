303ef6c3be2bedb0a090971f8a337ffcf60c7e21
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-15 15:35:24 -0300

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,9 @@
+//npm install -D typescript, tem q instalar no node, o -D só para desenvolvimento
+var idade = 27;
+var nome = 'Diogo';
+var maiorDeIdade = true;
+function soma(num1, num2) {
+    return num1 + num2;
+}
+document.write("A soma de 7 + 5 \u00E9 ".concat(soma(7, 5)));
+//comando para transpilar o typeScript tsc
